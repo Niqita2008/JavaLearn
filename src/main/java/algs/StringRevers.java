2e52fad493af122e61e022ade58e3c56c754bf8e
niqita00 <bondarenkonikita@gmail.com>
2021-01-12 23:55:47 +0500

    add test

--- src/main/java/algs/StringRevers.java
+++ src/main/java/algs/StringRevers.java
@@ -1,8 +1,11 @@
 package algs;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class StringRevers {
 
-    public static String revers(String s) {
+    public String revers(String s) {
         String result = "";
         if (s != null) {
             for (int i = 0; i < s.length(); i++) {
@@ -10,5 +13,15 @@ public class StringRevers {
             }
         }
         return result;
+    }
+
+    @Test
+    public void testRevers() {
+        String forRevers = "1234567890";
+        String expected = "87654321";
+        String actual;
+        StringRevers stringRevers = new StringRevers();
+        actual = stringRevers.revers(forRevers);
+        Assert.assertEquals("Реверс не работает", expected, actual);
     }
 }
